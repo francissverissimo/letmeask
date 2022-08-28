4dfed90f5dfcd6f6d01510130cf72416d92e672f
Francis Verissimo <francissv97@gmail.com>
2022-08-28 19:35:36 -0400

    first commit: use viteJS

---
+++ README.md
@@ -0,0 +1,18 @@
+<img alt="Project image cover" src=".github/letmeask.svg" width="100%" />
+
+<h2>Site published <a href="https://letmeask-3b93b.firebaseapp.com/">here</a></h2>
+
+## Technologies
+
+This project was developed with the following technologies:
+
+- React
+- Firebase: Auth & Realtime Database
+- Sass
+- Phosphor-React: Icons
+- Typescript
+- Vite JS
+
+## Project
+
+Letmeask is perfect for content creators to be able to create Q&A rooms with their audience, in a very organized and democratic way.
---
+++ src/pages/Home.tsx
@@ -0,0 +1,80 @@
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { database } from "../services/firebase";
+import { useAuth } from "../hooks/useAuth";
+import { Button } from "../components/Button";
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+import "../styles/auth.scss";
+
+export function Home() {
+  const navigate = useNavigate();
+  const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    navigate("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") return;
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+
+    if (roomRef.val().endedAt) {
+      alert("Esta sala já foi encerrada.");
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <div>
+          <img
+            src={illustrationImg}
+            alt="Ilustração simbolizando as respostas"
+          />
+          <strong>Crie salas de Q&amp;A ao-vivo</strong>
+          <p>Tire as dúvidas da sua audiência em tempo real</p>
+        </div>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">ou entre em uma sala</div>
+
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+              placeholder="Digite o código da sala"
+            />
+
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
